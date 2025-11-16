3cd6d532639a3f3b0faf1799c6989ea66440f974
ahmadqurniawan <ahmadqurniawan12@gmail.com>
2025-11-16 21:23:46 +0700

    modification: layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,10 @@
+// app/layout.tsx
 import type { Metadata } from "next";
 import { Raleway } from "next/font/google";
 import Navbar from "@/component/navbar/navbar";
-import Footer from "@/component/footer"
+import Footer from "@/component/footer";
+import Providers from "./providers";
 import "./globals.css";
-;
 
 const raleway = Raleway({
   variable: "--font-raleway",
@@ -15,19 +16,15 @@ export const metadata: Metadata = {
   description: "E-Commerce Website",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className={`${raleway.variable} antialiased`}>
-        <Navbar/>
-        <main className="bg-gray-50 min-h-screen">
-          {children}
-        </main>
-        <Footer/>
+        <Providers>
+          <Navbar />
+          <main className="bg-gray-50 min-h-screen">{children}</main>
+          <Footer />
+        </Providers>
       </body>
     </html>
   );
